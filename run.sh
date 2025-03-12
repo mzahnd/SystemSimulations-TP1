54d5cd986bdb081790b28ffab5adfb055e8898a6
FSendot <fsendot@itba.edu.ar>
2025-03-12 15:55:29 -0300

    [CODE] Refactor runner script to use variables

--- run.sh
+++ run.sh
@@ -1,20 +1,30 @@
+matrix_size=40
+output_directory=./example
+number_of_particles=3000
+board_side_length=20
+interaction_radius=1
+particle_radius=0.25
+periodic_countour=false
+
 clear
 gradle clean build
 gradle run --no-build-cache --rerun-tasks \
-          --args="--matrix-size=40 \
-                  --output-directory=./example \
-                  --number-of-particles=3000 \
-                  --board-side-length=20 \
-                  --interaction-radius=1 \
-                  --particle-radius=0.25 \
+          --args="--matrix-size=${matrix_size} \
+                  --output-directory=${output_directory} \
+                  --number-of-particles=${number_of_particles} \
+                  --board-side-length=${board_side_length} \
+                  --interaction-radius=${interaction_radius} \
+                  --particle-radius=${particle_radius} \
+                  --periodic-countour=${periodic_countour} \
                   --generate-random \
                   --algorithm=BRUTE_FORCE"
 gradle run --no-build-cache --rerun-tasks \
-          --args="--matrix-size=40 \
-                  --output-directory=./example \
-                  --number-of-particles=3000 \
-                  --board-side-length=20 \
-                  --interaction-radius=1 \
-                  --particle-radius=0.25 \
+          --args="--matrix-size=${matrix_size} \
+                  --output-directory=${output_directory} \
+                  --number-of-particles=${number_of_particles} \
+                  --board-side-length=${board_side_length} \
+                  --interaction-radius=${interaction_radius} \
+                  --particle-radius=${particle_radius} \
+                  --periodic-countour=${periodic_countour} \
                   --generate-random \
                   --algorithm=CELL_INDEX_METHOD"
